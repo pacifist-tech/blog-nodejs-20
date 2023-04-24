7ab7365c15f3792bea83cd5ad7d0723afc410696
Greg <mistzero0@gmail.com>
2023-04-24 10:53:49 +0800

    add execution on package.json

--- src/string-isWellFormed/string.js
+++ src/string-isWellFormed/string.js
@@ -1,2 +1,2 @@
-console.log("\uD83D\uDC36".isWellFormed()) // 🐶 -> true
-console.log("\uDC36\uD83D".isWellFormed()) // reversed 🐶 -> false
+console.log("\uD83D\uDC36 is well formed: ", "\uD83D\uDC36".isWellFormed()) // 🐶 -> true
+console.log("\uDC36\uD83D is well formed: ", "\uDC36\uD83D".isWellFormed()) // reversed 🐶 -> false
